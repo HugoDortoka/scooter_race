7ddde934453888470548593f46f90f4897988510
HugoDortoka <hugo.varela@inslapineda.cat>
2024-03-07 00:40:46 +0100

    bootstrap

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,13 +3,81 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 	<title>Admin Home</title>
+	<!-- CSS de Bootstrap -->
+	<link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+	<!-- Scripts de Bootstrap (JavaScript) -->
+	<script src="{{ asset('assets/js/bootstrap.min.js') }}" defer></script>
+	<!-- Additional CSS Files -->
+	<link rel="stylesheet" href="{{ asset('assets/css/fontawesome.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/css/templatemo-cyborg-gaming.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/css/owl.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}">
+	<link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+
 </head>
 <body>
-	<h1>Welcome to the Admin Panel</h1>
+<!-- ***** Preloader Start ***** -->
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+<header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="index.html" class="logo">
+                        <img src="assets/images/logo.png" alt="">
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Search End ***** -->
+                    <div class="search-input">
+                      <form id="search" action="#">
+                        <input type="text" placeholder="Type Something" id='searchText' name="searchKeyword" onkeypress="handle" />
+                        <i class="fa fa-search"></i>
+                      </form>
+                    </div>
+                    <!-- ***** Search End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                        <li> <a href="{{ route('admin.insurers') }}">Insurers</a></li>
+                        <li><a href="{{ route('admin.home') }}" class="active">Races</a></li>
+                        <li><a href="details.html">Sponsors</a></li>
+                        <li><a href="streams.html">Competitors</a></li>
+                        <li><a href="{{ route('admin.logout') }}">Logout<img src="assets/images/profile-header.jpg" alt=""></a></li>
+                    </ul>   
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+	
 	<!-- Aquí puedes agregar contenido adicional según sea necesario -->
-    <a href="{{ route('admin.insurers') }}">Insurers</a>
-	<a href="{{ route('admin.logout') }}">Logout</a>
+    
+<!-- Scripts -->
+<!-- Bootstrap core JavaScript -->
+<script src="{{ asset('assets/js/jquery/jquery.min.js') }}"></script>
+
+<script src="{{ asset('assets/js/isotope.min.js') }}"></script>
+<script src="{{ asset('assets/js/owl-carousel.js') }}"></script>
+<script src="{{ asset('assets/js/tabs.js') }}"></script>
+<script src="{{ asset('assets/js/popup.js') }}"></script>
+<script src="{{ asset('assets/js/custom.js') }}"></script>
+
 </body>
 </html>
 
--- resources/views/admin/insurers.blade.php
+++ resources/views/admin/insurers.blade.php
@@ -1,9 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <title>Admin Home</title>
+    <!-- CSS de Bootstrap -->
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="{{ asset('assets/css/admin.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/templatemo-cyborg-gaming.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/owl.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}">
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+</head>
+<body>
+    <!-- ***** Preloader Start ***** -->
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+<header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="index.html" class="logo">
+                        <img src="assets/images/logo.png" alt="">
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Search End ***** -->
+                    <div class="search-input">
+                        <form id="search" action="#">
+                            <input type="text" placeholder="Type Something" id='searchText' name="searchKeyword" onkeypress="handle" />
+                            <i class="fa fa-search"></i>
+                        </form>
+                    </div>
+                    <!-- ***** Search End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                        <li><a href="{{ route('admin.insurers') }}" class="active">Insurers</a></li>
+                        <li><a href="{{ route('admin.home') }}" >Races</a></li>
+                        <li><a href="details.html">Sponsors</a></li>
+                        <li><a href="streams.html">Competitors</a></li>
+                        <li><a href="{{ route('admin.logout') }}">Logout<img src="assets/images/profile-header.jpg" alt=""></a></li>
+                    </ul>   
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
 
-<a href="{{ route('admin.insurerShowAdd') }}"><button>Add</button></a>
+<!-- Contenido adicional -->
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="page-content">
 
-@foreach($insurers as $insurer)
-    <p>Name: {{ $insurer->name }}<a href="{{ route('admin.insurerShow', $insurer->id) }}"><button>Show details</button></a><a href="{{ route('admin.insurerChange', $insurer->id) }}"><button>Change active</button></a></p>
-    <hr>
-@endforeach
+                <a href="{{ route('admin.insurerShowAdd') }}" class="btn btn-pink btnAdd">Add</a>
+                @foreach($insurers as $insurer)
+                    <div class="row justify-content-center">
+                        <div>
+                            <div class="insurer">
+                                <h5>Name: {{ $insurer->name }}</h5>
+                                <a href="{{ route('admin.insurerShow', $insurer->id) }}" class="btn btn-pink mr-2">Show details</a>
+                                <a href="{{ route('admin.insurerChange', $insurer->id) }}" class="btn btn-pink">Change active</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</div>
 
+<!-- Scripts de Bootstrap (JavaScript) -->
+<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('assets/js/jquery/jquery.min.js') }}"></script>
 
+<script src="{{ asset('assets/js/isotope.min.js') }}"></script>
+<script src="{{ asset('assets/js/owl-carousel.js') }}"></script>
+<script src="{{ asset('assets/js/tabs.js') }}"></script>
+<script src="{{ asset('assets/js/popup.js') }}"></script>
+<script src="{{ asset('assets/js/custom.js') }}"></script>
+</body>
+</html>
