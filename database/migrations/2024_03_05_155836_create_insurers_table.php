6d6f94597bb438935a4317a7cc2568d62c435074
lucasmoreno12345 <lucas.moreno@inslapineda.cat>
2024-03-20 17:31:32 +0100

    inserts reales

--- database/migrations/2024_03_05_155836_create_insurers_table.php
+++ database/migrations/2024_03_05_155836_create_insurers_table.php
@@ -20,6 +20,51 @@ return new class extends Migration
             $table->boolean('active')->default(true);
             $table->timestamps();
         });
+
+        DB::table('insurers')->insert([
+            'CIF' => 'J40407488',
+            'name' => 'Adeslas',
+            'address' => '34 Security Avenue',
+            'price_per_course' => 23.00,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('insurers')->insert([
+            'CIF' => 'W3974150I',
+            'name' => 'Allianz',
+            'address' => '234 Tranquility Street',
+            'price_per_course' => 40.00,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('insurers')->insert([
+            'CIF' => 'U74739905',
+            'name' => 'Mutua',
+            'address' => '41 Precaution Drive',
+            'price_per_course' => 65.00,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('insurers')->insert([
+            'CIF' => 'H03599511',
+            'name' => 'MAPFRE',
+            'address' => '101 Secure Boulevard',
+            'price_per_course' => 18.00,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('insurers')->insert([
+            'CIF' => 'N0669939A',
+            'name' => 'Sanitas',
+            'address' => '67 Secure Avenue',
+            'price_per_course' => 50.00,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 
     /**
--- database/migrations/2024_03_05_160103_create_sponsors_table.php
+++ database/migrations/2024_03_05_160103_create_sponsors_table.php
@@ -22,6 +22,58 @@ return new class extends Migration
             $table->boolean('active')->default(true);
             $table->timestamps();
         });
+
+        DB::table('sponsors')->insert([
+            'CIF' => 'E18886895',
+            'name' => 'Adidas',
+            'logo' => 'img/sponsors/1710951867_Adidas.png',
+            'address' => '45 Main Street, Central City',
+            'principal' => true,
+            'extra_cost' => 50.00,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('sponsors')->insert([
+            'CIF' => 'C34293746',
+            'name' => 'Coca Cola',
+            'logo' => 'img/sponsors/1710951904_Coca Cola.png',
+            'address' => '12 Innovation Avenue, Tech District',
+            'principal' => true,
+            'extra_cost' => 50.00,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('sponsors')->insert([
+            'CIF' => 'D02144350',
+            'name' => 'VISA',
+            'logo' => 'img/sponsors/1710951946_VISA.png',
+            'address' => '3 Sun Street, Art Quarter',
+            'extra_cost' => null,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('sponsors')->insert([
+            'CIF' => 'E12554036',
+            'name' => 'MasterCard',
+            'logo' => 'img/sponsors/1710951994_MasterCard.png',
+            'address' => '67 Seafront Drive, Bright Coast',
+            'extra_cost' => null,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('sponsors')->insert([
+            'CIF' => 'U26485805',
+            'name' => 'McDonalds',
+            'logo' => 'img/sponsors/1710952026_McDonalds.png',
+            'address' => '5 Mayor Square, Historic Village',
+            'extra_cost' => null,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 
     /**
