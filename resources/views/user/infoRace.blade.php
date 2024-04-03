267b575fba95d7a8d549dc34fa21f9500782fb7f
HugoDortoka <hugo.varela@inslapineda.cat>
2024-04-03 19:34:13 +0200

    popup register

--- resources/views/user/infoRace.blade.php
+++ resources/views/user/infoRace.blade.php
@@ -15,6 +15,7 @@
     <!-- Scripts de Bootstrap (JavaScript) -->
     <script src="{{ asset('assets/js/bootstrap.min.js') }}" defer></script>
     <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="{{ asset('assets/css/admin.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/templatemo-cyborg-gaming.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/owl.css') }}">
@@ -116,23 +117,70 @@ https://templatemo.com/tm-579-cyborg-gaming
                       <div class="row mt-4">
                         <div class="col col-6"><p class="priceRace">Price: {{ $course->registration_price }}€</p></div>
                         <div class="col col-6"> 
-                          <div class="main-button">
+                          <div>
                             @if ($user==null)
-                              <a href="{{ route('user.register2', $course->id) }}">Register</a>
-                              <a href="#myModal" class="trigger-modal">Abrir Modal</a>
+                              <a href="{{ route('user.register2', $course->id) }}" class="btn btn-pink">Register</a>
+                              <a href="#myModal" class="trigger-modal btn btn-pink">Abrir Modal</a>
 
-                              <div id="myModal" style="display: none;" class="mt-4">
+                              <div id="myModal" style="display: none;">
                                   <!-- Contenido de tu modal aquí -->
-                                  <h2>Mi Modal</h2>
-                                  <p>Contenido del modal...</p>
-                                  <button class="close-modal">Cerrar</button>
+                                  <div class="centrado">
+                                    
+                                    <form action="{{ route('user.insertRegistration') }}" method="POST" class="formRegister" onsubmit="return validateForm()">
+                                        @csrf
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label for="dni" class="labelLogin">DNI</label><br>
+                                                <div class="divLogin" id="divDNI">
+                                                    <input type="text" id="dni" name="dni" class="inputLogin" placeholder="Type your DNI" required>
+                                                </div>
+                                                <label for="name" class="labelLogin">Name</label><br>
+                                                <div class="divLogin">
+                                                    <input type="text" id="name" name="name" class="inputLogin" placeholder="Type your name" required>
+                                                </div>
+                                                <label for="surname" class="labelLogin">Surname</label><br>
+                                                <div class="divLogin">
+                                                    <input type="text" id="surname" name="surname" class="inputLogin" placeholder="Type your surname" required>
+                                                </div>
+                                                <label for="address" class="labelLogin">Address</label><br>
+                                                <div class="divLogin">
+                                                    <input type="text" id="address" name="address" class="inputLogin" placeholder="Type your address" required>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <label for="birth" class="labelLogin">Date of birth</label><br>
+                                                <div class="divLogin">
+                                                    <input type="date" id="birth" name="birth" class="inputLogin" max="{{ date('Y-m-d', strtotime('-1 day')) }}" required>
+                                                </div>
+                                                <label for="PRO_OPEN" class="labelLogin">What are you?</label><br>
+                                                <div class="divLogin">
+                                                    <select id="PRO_OPEN" name="PRO_OPEN" class="inputLogin" required>
+                                                        <option value="OPEN">OPEN</option>
+                                                        <option value="PRO">PRO</option>
+                                                    </select>
+                                                </div>
+                                                <div id="federationInput" style="display: none;">
+                                                    <label for="federation" class="labelLogin">Federation number</label><br>
+                                                    <div class="divLogin" id="divFederation">
+                                                        <input type="text" id="federation" name="federation" class="inputLogin" placeholder="Type your fed. number">
+                                                    </div>
+                                                </div>
+                                                <label for="password" class="labelLogin">Password</label><br>
+                                                <div class="divLogin">
+                                                    <input type="password" id="password" name="password" class="inputLogin" placeholder="Type your password" required>
+                                                </div>
+                                            </div>
+                                            <input type="submit" value="Register" class="btn btn-pink btnAdd btnLogin close-modal">
+                                        </div>
+                                    </form>
+                                </div>
                               </div>
                             @else
                               @if ($registration->contains('competitor_id', $user->id))
                                 <p class="priceRace">Registered</p>
                               @else
                                 @if ($user->PRO==true)
-                                  <a href="{{ route('user.register2', $course->id) }}">Register</a>              
+                                  <a href="{{ route('user.register2', $course->id) }}" class="btn btn-pink">Register</a>              
                                 @else
                                   <form action="{{ route('user.register', $course->id) }}"  method="POST" >
                                     @csrf
@@ -200,6 +248,8 @@ https://templatemo.com/tm-579-cyborg-gaming
 
 
   <!-- Scripts -->
+  <script src="{{ asset('assets/js/federation.js') }}"></script>
+  <script src="{{ asset('assets/js/validation.js') }}"></script>
   <!-- Bootstrap core JavaScript -->
   <script src="{{ asset('assets/js/jquery/jquery.min.js') }}"></script>
 
